bdba728ff7eeeb201d04c0c2666323bd122cc57a
matthewschuler <matthewschuler@localhost>
2024-09-24 11:41:23 -0400

    schedule

---
+++ ICS3U-SchulerMatthew/src/Unit1/AboutMe.java
@@ -0,0 +1,45 @@
+package Unit1;
+/**
+* Description: This program prints My name, grade, teacher, and school,
+* a second statement. <br>
+* Date: Sept. 20, 2024
+* @author Matthew Schuler
+*/
+public class AboutMe {
+	static int w = 20;
+	public static void main(String[] args) {
+	
+	
+	System.out.println("                                ~~~~~~~~~~~~");           
+	System.out.println("                                | About Me |");           
+	System.out.println("                                ~~~~~~~~~~~~");            
+	System.out.println("                        |My name is Matthew Schuler|");
+	System.out.println("                             |I am in grade 11|");       
+	System.out.println("                          |My teacher is Ms. Kemp|");      
+	System.out.println("                         |I go to Lawrence Park CI|");    
+	System.out.println("                               ");
+	System.out.println("==========     ========     ========           ==         ========    ==========");
+	System.out.println("||      ||    ||           ||      ||          ||         ||     ||   ||      ||");
+	System.out.println(" ||    ||     ||           ||      ||          ||         ||     ||    ||    ||");
+	System.out.println("  ||  ||      ||           ||      ||          ||         ||     ||     ||  ||");
+	System.out.println("   ||||       ||    ====== ||      ||          ||         ||======       ||||");
+	System.out.println("    ||        ||      ||   ||      ||          ||         ||              ||");
+	System.out.println("    --        ||      ||   ||      ||          ||         ||              --");
+	System.out.println("    {}         ========     ========           =========  ==              {}");
+	System.out.println("")
+	System.out.println("===============================================================================");
+	System.out.println("||        TIME         ||              MONDAY               || TUESDAY || WEDNESDAY || THURSDAY || FRIDAY||                                                                           ||");
+	System.out.println("||---------------------||-----------------------------------||-----------------||                                                                      ||");
+	System.out.println("|| 09:00 AM - 10:15 AM || Computer and Information Science  ||                                                     ||");
+	System.out.println("||---------------------||-----------------------------------||                  ||");
+	System.out.println("|| 10:20 AM - 11:40 AM || Financial Accounting Fundamentals ||                   ||");
+	System.out.println("||---------------------||                                  ||                   ||");
+	System.out.println("|| 12:40 PM - 01:55 PM ||                                  ||                   ||");
+	System.out.println("||---------------------||                                  ||                   ||");
+	System.out.println("|| 02:00 PM - 03:15 PM ||                                  ||                   ||");
+	System.out.println("==============================================================================||");
+	
+
+	
+	}
+}
