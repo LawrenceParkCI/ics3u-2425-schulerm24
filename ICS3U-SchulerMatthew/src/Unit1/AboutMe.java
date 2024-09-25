78d80eedf54f94372583e5d93d19c39dfa673568
matthewschuler <matthewschuler@localhost>
2024-09-25 10:18:41 -0400

    FINISHED

--- ICS3U-SchulerMatthew/src/Unit1/AboutMe.java
+++ ICS3U-SchulerMatthew/src/Unit1/AboutMe.java
@@ -10,34 +10,48 @@ public class AboutMe {
 	public static void main(String[] args) {
 	
 	
-	System.out.println("                                ~~~~~~~~~~~~");           
-	System.out.println("                                | About Me |");           
-	System.out.println("                                ~~~~~~~~~~~~");            
-	System.out.println("                        |My name is Matthew Schuler|");
-	System.out.println("                             |I am in grade 11|");       
-	System.out.println("                          |My teacher is Ms. Kemp|");      
-	System.out.println("                         |I go to Lawrence Park CI|");    
-	System.out.println("                               ");
-	System.out.println("==========     ========     ========           ==         ========    ==========");
-	System.out.println("||      ||    ||           ||      ||          ||         ||     ||   ||      ||");
-	System.out.println(" ||    ||     ||           ||      ||          ||         ||     ||    ||    ||");
-	System.out.println("  ||  ||      ||           ||      ||          ||         ||     ||     ||  ||");
-	System.out.println("   ||||       ||    ====== ||      ||          ||         ||======       ||||");
-	System.out.println("    ||        ||      ||   ||      ||          ||         ||              ||");
-	System.out.println("    --        ||      ||   ||      ||          ||         ||              --");
-	System.out.println("    {}         ========     ========           =========  ==              {}");
-	System.out.println("")
-	System.out.println("===============================================================================");
-	System.out.println("||        TIME         ||              MONDAY               || TUESDAY || WEDNESDAY || THURSDAY || FRIDAY||                                                                           ||");
-	System.out.println("||---------------------||-----------------------------------||-----------------||                                                                      ||");
-	System.out.println("|| 09:00 AM - 10:15 AM || Computer and Information Science  ||                                                     ||");
-	System.out.println("||---------------------||-----------------------------------||                  ||");
-	System.out.println("|| 10:20 AM - 11:40 AM || Financial Accounting Fundamentals ||                   ||");
-	System.out.println("||---------------------||                                  ||                   ||");
-	System.out.println("|| 12:40 PM - 01:55 PM ||                                  ||                   ||");
-	System.out.println("||---------------------||                                  ||                   ||");
-	System.out.println("|| 02:00 PM - 03:15 PM ||                                  ||                   ||");
-	System.out.println("==============================================================================||");
+	System.out.println("                                                                                                 ~~~~~~~~~~~~");           
+	System.out.println("                                                                                                 | About Me |");           
+	System.out.println("                                                                                                 ~~~~~~~~~~~~");            
+	System.out.println("                                                                                         |My name is Matthew Schuler|");
+	System.out.println("                                                                                              |I am in grade 11|");       
+	System.out.println("                                                                                           |My teacher is Ms. Kemp|");      
+	System.out.println("                                                                                          |I go to Lawrence Park CI|                                                  ");    
+	System.out.println("                                                                                                   |GO LP!|");
+	System.out.println("  ");
+	System.out.println("                                                                                    ==          =======     =======   ========");
+	System.out.println("                                                                                    ||         ||     ||   ||     ==     ||");
+	System.out.println("                                                                                    ||         ||     ||  ||             ||");
+	System.out.println("                                                                                    ||         ||     ||  ||             ||");
+	System.out.println("                                                                                    ||         ||======   ||             ||");
+	System.out.println("                                                                                    ||         ||         ||             ||");
+	System.out.println("                                                                                    ||         ||          ||     ==     ||");
+	System.out.println("                                                                                    =========  ==           =======   ========");
+	System.out.println(" ");
+	
+	System.out.println("                              ==============   ==============   ====             ====   ============    ==============         ===           ========     ==           ============                               ");
+	System.out.println("                                    ||               ||          ||\\            //||    ||                    ||               //\\          ||       \\    ||           ||                                      ");
+	System.out.println("                                    ||               ||          || \\          // ||    ||                    ||              //  \\         ||       ||   ||           ||                                       ");
+	System.out.println("                                    ||               ||          ||  \\        //  ||    ||                    ||             //    \\        ||       //   ||           ||                                       ");
+	System.out.println("                                    ||               ||          ||   \\      //   ||    ||=========           ||            //======\\       ||=======\\    ||           ||========                              ");
+	System.out.println("                                    ||               ||          ||    \\    //    ||    ||                    ||           //        \\      ||        \\   ||           ||                                      ");
+	System.out.println("                                    ||               ||          ||     \\  //     ||    ||                    ||          //          \\     ||        ||  ||           ||                                       ");
+	System.out.println("                                    ||               ||          ||      \\//      ||    ||                    ||         //            \\    ||        //  ||           ||                                       ");
+	System.out.println("                                   ====        ==============   ====    ====     ====   ============         ====      ====           ====   =========    ==========   ============                               ");
+	System.out.println("");
+	System.out.println("==================================================================================================================================================================================================================");
+	System.out.println("||        TIME         ||              MONDAY               ||              TUESDAY              ||             WEDNESDAY             ||             THURSDAY              ||              FRIDAY               ||");
+	System.out.println("||-~~~~~~~~~~~~~~~~~~~~||-------------~~~~~~~~--------------||-------------~~~~~~~~~-------------||------------~~~~~~~~~~~------------||------------~~~~~~~~~~-------------||-------------~~~~~~~~--------------||");
+	System.out.println("|| 09:00 AM - 10:15 AM || Computer and Information Sciences || Financial Accounting Fundamentals || Computer and Information Sciences || Financial Accounting Fundamentals || Computer and Information Sciences ||");
+	System.out.println("||---------------------||-----------------------------------||-----------------------------------||-----------------------------------||-----------------------------------||-----------------------------------||");
+	System.out.println("|| 10:20 AM - 11:40 AM || Financial Accounting Fundamentals || Computer and Information Sciences || Financial Accounting Fundamentals || Computer and Information Sciences || Financial Accounting Fundamentals ||");
+	System.out.println("||---------------------||-----------------------------------||-----------------------------------||-----------------------------------||-----------------------------------||-----------------------------------||");
+	System.out.println("|| 11:40 AM - 12:40 PM ||               LUNCH               ||               LUNCH               ||               LUNCH               ||               LUNCH               ||               Lunch               ||");
+	System.out.println("||---------------------||-----------------------------------||-----------------------------------||-----------------------------------||-----------------------------------||-----------------------------------||");
+	System.out.println("|| 12:40 PM - 01:55 PM ||             Functions             ||                                   ||             Functions             ||                                   ||             Functions             ||");
+	System.out.println("||---------------------||-----------------------------------||-----------------------------------||-----------------------------------||-----------------------------------||-----------------------------------||");
+	System.out.println("|| 02:00 PM - 03:15 PM ||                                   ||             Functions             ||                                   ||             Functions             ||                                   ||");
+	System.out.println("==================================================================================================================================================================================================================");
 	
 
 	
