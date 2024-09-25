4dea22b9226db12da929f78f0f764d217fffdedc
matthewschuler <matthewschuler@localhost>
2024-09-25 11:02:34 -0400

    Worked on Bingo title

---
+++ ICS3U-SchulerMatthew/src/Unit1/Bingo.java
@@ -0,0 +1,37 @@
+package Unit1;
+/**
+* Description: This program prints My name, grade, teacher, and school,
+* a second statement. <br>
+* Date: Sept. 20, 2024
+* @author Matthew Schuler
+*/
+public class Bingo {
+	static int w = 20;
+	public static void main(String[] args) {
+	
+	
+	System.out.println("|     =========        |     ============     |   ==        ==   |    ==========    |    ==========    |");
+	System.out.println("|     ||       \\       |          ||          |   ||\\       ||   |   ||             |   ||        ||   |");
+	System.out.println("|     ||       ||      |          ||          |   || \\      ||   |   ||             |   ||        ||   |");
+	System.out.println("|     ||       ||      |          ||          |   ||  \\     ||   |   ||             |   ||        ||   |");
+	System.out.println("|     ||=======\\       |          ||          |   ||   \\    ||   |   ||             |   ||        ||   |");
+	System.out.println("|     ||        \\      |          ||          |   ||    \\   ||   |   ||      ====   |   ||        ||   |");
+	System.out.println("|     ||        ||     |          ||          |   ||     \\  ||   |   ||       ||    |   ||        ||   |");
+	System.out.println("|     ||        ||     |          ||          |   ||      \\ ||   |   ||       ||    |   ||        ||   |");
+	System.out.println("|     ||        //     |          ||          |   ||       \\||   |   ||       ||    |   ||        ||   |");
+	System.out.println("|     ===========      |     ============     |   ==        ==   |    =========     |    ==========    |");
+	System.out.println("|     ~~~~~~~~~~~~     |     ~~~~~~~~~~~~     |   ~~~~~~~~~~~~   |   ~~~~~~~~~~~~   |   ~~~~~~~~~~~~   |");
+	System.out.println("------------------------------------------------------------------------------------------------");
+	System.out.println("");
+	System.out.println("");
+	System.out.println("");
+	System.out.println("");
+	System.out.println("");
+	System.out.println("");
+	System.out.println("");
+	System.out.println("");
+	System.out.println("");
+	System.out.println("");
+	System.out.println("");
+	}
+}
