622871b5dd768f5e8ac5483531fc9b4848894265
matthewschuler <matthewschuler@localhost>
2024-10-22 11:10:39 -0400

    made reciept show quantitiy

--- ICS3U-SchulerMatthew/src/Unit1/shopping.java
+++ ICS3U-SchulerMatthew/src/Unit1/shopping.java
@@ -9,8 +9,14 @@ public class shopping {
 
 		Scanner sc = new Scanner(System.in);
 	
+		
+		//food items
+		
 		double AsparagusQuant, AvocadoQuant, BeetQuant, BroccoliQuant, CabbageQuant, CarrotQuant, CauliflowerQuant, CeleryQuant, CornQuant, CucumberQuant, GarlicQuant, GreenBeanQuant, OnionQuant, PeaQuant, PepperQuant, PotatoQuant, TomatoQuant;
 
+		
+		//purchase amounts
+		
 		System.out.println("                   Welcome To VEGETABLE VARIETY                   ");
 		System.out.println("                   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~                   ");
 		System.out.println();
@@ -101,11 +107,7 @@ public class shopping {
 		System.out.println();
 	
 
-	
-	
-	
-
-	
+	//food prices	
 
 	double Asparagus = 1.50;
 	double Avocado = 1.75;
@@ -125,6 +127,9 @@ public class shopping {
 	double Potato = 2.75;
 	double Tomato = 1.75;
 
+	
+	//food cost total
+	
 	double AsparagusCost = Asparagus * AsparagusQuant;
 	double AvocadoCost = Avocado * AvocadoQuant;
 	double BeetCost = Beet * BeetQuant;
@@ -144,6 +149,8 @@ public class shopping {
 	double TomatoCost = Tomato * TomatoQuant;
 	
 	
+	//total
+	
 	double SubTotal = AsparagusCost + AvocadoCost + BeetCost + BroccoliCost + CabbageCost + CarrotCost + CauliflowerCost + CeleryCost + CornCost + CucumberCost + GarlicCost + GreenBeanCost + OnionCost + PeaCost + PepperCost + PotatoCost + TomatoCost;
 	double Tax = SubTotal * 0.13;
 	double Total = (SubTotal * 0.13) + SubTotal;
@@ -151,7 +158,7 @@ public class shopping {
 	sc.close();
 	
 	
-	System.out.println("                     --------------------------\n                     --------------------------\n                          VEGITABLE VARIETY \n                     --------------------------\n                     --------------------------\n \n                      Asparagus          $" + (Math.round(AsparagusCost * 100.0) / 100.0) + "\n                      Avocado            $" + (Math.round(AvocadoCost * 100.0) / 100.0) + "\n                      Beet               $" + (Math.round(BeetCost * 100.0) / 100.0) + "\n                      Broccoli           $" + (Math.round(BroccoliCost * 100.0) / 100.0) + "\n                      Cabbage            $" + (Math.round(CabbageCost * 100.0) / 100.0) + "\n                      Carrot             $" + (Math.round(CarrotCost * 100.0) / 100.0) + "\n                      Cauliflower        $" + (Math.round(CauliflowerCost * 100.0) / 100.0) + "\n                      Celery             $" + (Math.round(CeleryCost * 100.0) / 100.0) + "\n                      Corn               $" + (Math.round(CornCost * 100.0) / 100.0) + "\n                      Cucumber           $" + (Math.round(CucumberCost * 100.0) / 100.0) + "\n                      Garlic             $" + (Math.round(GarlicCost * 100.0) / 100.0) + "\n                      Green Bean         $" + (Math.round(GreenBeanCost * 100.0) / 100.0) + "\n                      Onion              $" + (Math.round(OnionCost * 100.0) / 100.0) + "\n                      Pea                $" + (Math.round(PeaCost * 100.0) / 100.0) + "\n                      Pepper             $" + (Math.round(PepperCost * 100.0) / 100.0) + "\n                      Potato             $" + (Math.round(PotatoCost * 100.0) / 100.0) + "\n                      Tomato             $" + (Math.round(TomatoCost * 100.0) / 100.0) + "\n                     -------------------------- \n \n                      SubTotal           $" + (Math.round(SubTotal * 100.0) / 100.0) + "\n                      Tax                $" + (Math.round(Tax * 100.0) / 100.0) + "\n                      Total              $" + (Math.round(Total * 100.0) / 100.0) + "\n\n                     -------------------------- \n                             THANK YOU \n                     --------------------------");
+	System.out.println("                     -------------------------\n                     -------------------------\n                         VEGITABLE VARIETY \n                     -------------------------\n                     -------------------------\n \n                      Asparagus x"+AsparagusQuant+"\t$" + (Math.round(AsparagusCost * 100.0) / 100.0) + "\n                      Avocado x"+AvocadoQuant+"\t$" + (Math.round(AvocadoCost * 100.0) / 100.0) + "\n                      Beet x"+BeetQuant+"\t\t$" + (Math.round(BeetCost * 100.0) / 100.0) + "\n                      Broccoli x"+BroccoliQuant+"\t$" + (Math.round(BroccoliCost * 100.0) / 100.0) + "\n                      Cabbage x"+CabbageQuant+"\t$" + (Math.round(CabbageCost * 100.0) / 100.0) + "\n                      Carrot x"+CarrotQuant+"\t$" + (Math.round(CarrotCost * 100.0) / 100.0) + "\n                      Cauliflower x"+CauliflowerQuant+"\t$" + (Math.round(CauliflowerCost * 100.0) / 100.0) + "\n                      Celery x"+CeleryQuant+"\t$" + (Math.round(CeleryCost * 100.0) / 100.0) + "\n                      Corn x"+CornQuant+"\t\t$" + (Math.round(CornCost * 100.0) / 100.0) + "\n                      Cucumber x"+CucumberQuant+"\t$" + (Math.round(CucumberCost * 100.0) / 100.0) + "\n                      Garlic x"+GarlicQuant+"\t$" + (Math.round(GarlicCost * 100.0) / 100.0) + "\n                      Green Bean x"+GreenBeanQuant+"\t$" + (Math.round(GreenBeanCost * 100.0) / 100.0) + "\n                      Onion x"+OnionQuant+"\t$" + (Math.round(OnionCost * 100.0) / 100.0) + "\n                      Pea x"+PeaQuant+"\t\t$" + (Math.round(PeaCost * 100.0) / 100.0) + "\n                      Pepper x"+PepperQuant+"\t$" + (Math.round(PepperCost * 100.0) / 100.0) + "\n                      Potato x"+PotatoQuant+"\t$" + (Math.round(PotatoCost * 100.0) / 100.0) + "\n                      Tomato x"+TomatoQuant+"\t$" + (Math.round(TomatoCost * 100.0) / 100.0) + "\n\n                     -------------------------- \n \n                      SubTotal          $" + (Math.round(SubTotal * 100.0) / 100.0) + "\n                      Tax               $" + (Math.round(Tax * 100.0) / 100.0) + "\n                      Total             $" + (Math.round(Total * 100.0) / 100.0) + "\n\n                     -------------------------- \n                             THANK YOU \n                     --------------------------");
 	
 	
 	
