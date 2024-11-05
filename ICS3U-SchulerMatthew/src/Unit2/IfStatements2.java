b082d9b5ec7a476b90ef7dd35d5502f3adc78f49
matthewschuler <matthewschuler@localhost>
2024-11-05 09:33:28 -0500

    made the program work

---
+++ ICS3U-SchulerMatthew/src/Unit2/IfStatements2.java
@@ -0,0 +1,48 @@
+package Unit2;
+
+import java.util.Scanner;
+
+public class IfStatements2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("What is your age?");
+		
+		int userAge = sc.nextInt();
+ 
+			    if (userAge > 18) { 
+			      System.out.println("You are an Adult!");
+			    } else {  
+			      System.out.println("Sorry, not quite yet.");
+			    }
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}
+
+}
