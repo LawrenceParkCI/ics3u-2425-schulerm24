90ce0160b017cfc2075ef9eb32b2c233d21a3144
matthewschuler <matthewschuler@localhost>
2024-11-13 09:54:23 -0500

    finished

---
+++ ICS3U-SchulerMatthew/src/Unit2/IfChallenge.java
@@ -0,0 +1,59 @@
+package Unit2;
+
+import java.util.Scanner;
+
+public class IfChallenge {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner (System.in);
+	
+		int Score = 0;
+		
+	System.out.println("What is the largest planet in our solar system? \n 1) Jupiter \n 2) Saturn \n 3) Urnaus \n 4) Neptune");
+	int Q1 = sc.nextInt();
+	if (Q1 == 1) {
+		
+		Score ++;
+	}
+	
+	System.out.println("Which is the only known planet to be inhabited solely by robots? \n 1) Mercury \n 2) Venus \n 3) Earth \n 4) Mars");
+	int Q2 = sc.nextInt();
+	if (Q2 == 4) {
+		
+		Score ++;
+	}
+	
+	System.out.println("NASA just launched a mission to search for life on a moon of which planet? \n 1) Mars \n 2) Jupiter \n 3) Saturn \n 4) Uranus");
+	int Q3 = sc.nextInt();
+	if (Q3 == 2) {
+		
+		Score ++;
+	}
+	
+	System.out.println("Your Score: " + Score + "/3");
+	switch(Score) {
+	case 0:
+		System.out.println("You failed! Better luck next time!");
+        break;
+      case 1:
+    	System.out.println("You failed! Better luck next time!");
+        break;
+      case 2:
+        System.out.println("You passed! Good Job!");
+        break;
+      case 3:
+        System.out.println("You got every question correct! Great job!");
+        break;
+		
+	}
+	
+	
+	
+	
+	
+	
+	}
+
+}
